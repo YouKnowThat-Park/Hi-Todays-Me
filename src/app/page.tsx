efa4b528c7f10de49ebcae233b3bb56ad7f3fad0
wooseok <youkn0wthat@naver.com>
2025-05-06 17:44:34 +0900

    : style :  디자인 수정 (디자인  사용하는 페이지 위치 변경)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import React from "react";
-import TodoInput from "./todo-List/_components/TodoInput";
-import TodoList from "./todo-List/_components/TodoList";
 import TodoListPage from "./todo-List/Todo";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className="w-[500px] h-[500px] ml-[100px] mt-[100px] border">
-      <h2 className="my-2 flex justify-center items-center">To Do List</h2>
+    <div>
       <TodoListPage />
     </div>
   );
 };
 
-export default page;
+export default Page;
