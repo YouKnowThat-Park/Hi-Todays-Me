eb7b547ddf9d769d913f85ad9efab0d6f721fc12
wooseok <youkn0wthat@naver.com>
2025-05-06 16:49:54 +0900

    : refactor : TodoInput 및 TodoList 구조 분리 및 페이지 구성 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
+import TodoInput from "./todo-List/_components/TodoInput";
+import TodoList from "./todo-List/_components/TodoList";
+import TodoListPage from "./todo-List/Todo";
 
 const page = () => {
   return (
     <div className="w-[500px] h-[500px] ml-[100px] mt-[100px] border">
       <h2 className="my-2 flex justify-center items-center">To Do List</h2>
-      <TodoInput />
-      <TodoList />
+      <TodoListPage />
     </div>
   );
 };
