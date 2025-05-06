9bb14988720232bbe8810fa007fcc2a625ab0213
wooseok <youkn0wthat@naver.com>
2025-05-06 17:45:27 +0900

    : style : 디자인 수정 (디자인 사용 위치 변경)

--- src/app/todo-List/Todo.tsx
+++ src/app/todo-List/Todo.tsx
@@ -4,10 +4,17 @@ import TodoList from "./_components/TodoList";
 
 export default function TodoListPage() {
   return (
-    <div>
-      <TodoInput />
-      <TodoList />
-      <EditTodoModal />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 ">
+      <div className="w-[500px] h-[600px] p-6 bg-white dark:bg-neutral-800 rounded-lg shadow-md flex flex-col">
+        <h2 className="text-2xl font-bold text-center mb-4 text-gray-800 dark:text-white">
+          To Do List
+        </h2>
+        <TodoInput />
+        <div className="flex-1 overflow-y-auto">
+          <TodoList />
+        </div>
+        <EditTodoModal />
+      </div>
     </div>
   );
 }
