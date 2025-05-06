2af0e26468ad632786f3c3d1e2f47ec662116ddd
wooseok <youkn0wthat@naver.com>
2025-05-06 14:30:01 +0900

    : feat : Recoil atom Todo list 설정

---
+++ src/recoil/atom.ts
@@ -0,0 +1,45 @@
+import { atom } from "recoil";
+
+export type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+export const isHydratedState = atom({
+  key: "isHydratedState",
+  default: false,
+});
+
+export const userState = atom({
+  key: "userState",
+  default: null as null | { id: number; name: string },
+});
+
+export const todoListState = atom<Todo[]>({
+  key: "todoListState",
+  default: [],
+  effects_UNSTABLE: [
+    ({ setSelf, onSet }) => {
+      if (typeof window === "undefined") return;
+
+      const saved = localStorage.getItem("todos");
+      if (saved) {
+        try {
+          setSelf(JSON.parse(saved));
+        } catch (e) {
+          console.error("invalid localStorage format");
+        }
+      }
+
+      onSet((newValue) => {
+        localStorage.setItem("todos", JSON.stringify(newValue));
+      });
+    },
+  ],
+});
+
+export const filterState = atom<"all" | "active" | "completed">({
+  key: "filterState",
+  default: "all",
+});
