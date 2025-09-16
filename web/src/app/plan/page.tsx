63121cfefa35ba95c734d3f5aa0c09b6b122e4fd
wooseok <youkn0wthat@naver.com>
2025-09-16 13:33:32 +0900

    style: Home & Business Time 디자인 틀 생성

--- web/src/app/plan/page.tsx
+++ web/src/app/plan/page.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import ViewMembershipForm from "./_components/ViewMembershipForm";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  flex flex-col justify-center items-center">
+      <div className="flex w-full">
+        <span className="lg:w-1/3">일반 멤버십 무료</span>
+        <span className="lg:w-1/3">프리미엄 멤버십 5,900원</span>
+        <span className="lg:w-1/3">기업형 팀 멤버십 99,000원</span>
+      </div>
+      <ViewMembershipForm />
+    </div>
+  );
 }
