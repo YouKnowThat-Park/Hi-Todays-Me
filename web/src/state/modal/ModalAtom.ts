9620a94d20282735e48f52e8ef8c76239fc2fb34
wooseok <youkn0wthat@naver.com>
2025-09-20 01:48:24 +0900

    🍻 feat: Calendar Modal Atom 추가

--- web/src/state/modal/ModalAtom.ts
+++ web/src/state/modal/ModalAtom.ts
@@ -5,3 +5,11 @@ export const editTodoModalState = atom<{ isOpen: boolean; todo: Todo | null }>({
   key: "editTodoModalState",
   default: { isOpen: false, todo: null },
 });
+
+export const calendarModal = atom<{
+  isOpen: boolean;
+  selectedDate: string | null;
+}>({
+  key: "calendarModalState",
+  default: { isOpen: false, selectedDate: null },
+});
