25b8af85b9ba780faef2298293d2fc03969c4ea6
wooseok <youkn0wthat@naver.com>
2025-09-20 01:38:35 +0900

    🍻 feat: Calendar Modal 생성

--- web/src/components/layout/calendar/CalendarIcon.tsx
+++ web/src/components/layout/calendar/CalendarIcon.tsx
@@ -1,9 +1,20 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 import { FiCalendar } from "react-icons/fi";
 
 export default function CalendarIcon() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const openCalendar = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("modal", "calendar");
+    router.push(`?${params.toString()}`);
+  };
   return (
     <button
+      onClick={openCalendar}
       className="
             fixed right-26 bottom-15
             w-14 h-14 
---
+++ web/src/components/layout/calendar/CalendarModal.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+export default function CalendarModal() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const isOpen = searchParams.get("modal") === "calendar";
+
+  if (!isOpen) return null;
+
+  const handleClose = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("modal");
+    router.push(`?${params.toString()}`);
+  };
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+      <div className="bg-white w-[500px]">
+        <button
+          onClick={handleClose}
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          닫기
+        </button>
+      </div>
+      CalendarModal
+    </div>
+  );
+}
